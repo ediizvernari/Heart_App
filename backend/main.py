b507a413b5a5b0675d15d56e312c6b64ad7537d0
ediizvernari <edi.izvernari@yahoo.com>
2025-02-15 18:31:18 +0200

    Added CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from backend.database import engine, Base, get_db
 from backend import crud, schemas
 from typing import List
 from backend.routers import users
+from fastapi.middleware.cors import CORSMiddleware
 
 async def create_tables():
     async with engine.begin() as conn:
@@ -15,7 +16,22 @@ app = FastAPI()
 async def on_startup():
     await create_tables()
 
+origins = [
+    "http://10.0.2.2",
+    "http://10.0.2.2:8000",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
 app.add_event_handler("startup", on_startup)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
